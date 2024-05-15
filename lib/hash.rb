356f71bce4b66b15114a75f4d42c490fb9d83955
Oleksii Kuts <hapt.com.ua@gmail.com>
2024-05-15 17:42:01 +0300

    hash map finished

--- lib/hash.rb
+++ lib/hash.rb
@@ -29,7 +29,64 @@ class HashMap
       @buckets[bucket_index].append({key => value})
     end
   end
+
+  def get(key)
+    bucket_index = hash(key) % @capacity
+    if pointer = key_pointer(@buckets[bucket_index], key)
+      pointer.value[key]
+    else
+      nil
+    end
+  end
+
+  def has?(key)
+    get(key) ? true : false
+  end
+
+  def remove(key)
+    # if key is not present in HashMap
+    return nil if !has?(key)
+
+    bucket_index = hash(key) % @capacity
+    pointer = @buckets[bucket_index].head
+
+    # if key stored in the head of LinkedList
+    if pointer.value[key]
+      @buckets[bucket_index].head = pointer.next_node
+      @buckets[bucket_index].tail = nil if pointer.next_node.nil?
+      return pointer.value[key]
+    end
+
+    while pointer do
+      next_pointer = pointer.next_node
+
+      if next_pointer.value[key]
+        pointer.next_node = next_pointer.next_node
+        @buckets[bucket_index].tail = pointer if pointer.next_node.nil?
+        return next_pointer.value[key]
+      end
+      pointer = pointer.next_node
+    end
+  end
+
+  def length
+    @buckets.map(&:size).sum
+  end
+
+  def clear!
+    @buckets.fill(nil)
+  end
+
+  def keys
+    flatten.keys
+  end
+
+  def values
+    flatten.values
+  end
+
 private
+
   def key_pointer(list, key)
     pointer = list.head
     while pointer do
@@ -39,11 +96,30 @@ private
 
     return nil
   end
+
+  def flatten
+    res = Hash.new
+    @buckets.each do |ll| # linked_list
+      pointer = ll.head
+      while pointer do
+        res.merge!(pointer.value)
+        pointer = pointer.next_node
+      end
+    end
+
+    return res
+  end
 end
+
+
 my_hash = HashMap.new
 my_hash.set('name1', 'Lexus')
 my_hash.set('name2', 'Kuts')
 my_hash.set('name22', 'Oleksii')
-pp my_hash
 my_hash.set('name2', 'Oleksii Kuts')
 pp my_hash
+# pp my_hash.remove('name22').inspect
+# pp my_hash
+pp my_hash.length
+pp my_hash.keys
+pp my_hash.values
